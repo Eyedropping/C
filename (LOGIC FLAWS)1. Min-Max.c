53397f77c4e76f688e97c4393c2a3968ea9a4f89
Dmitry Aksyonov <dmi.aksyonov@gmail.com>
2019-09-04 00:02:28 +0300

    initial UL

---
+++ (LOGIC FLAWS)1. Min-Max.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void findV(int *arr)
+{
+    int min = 999;
+    int max = 0;
+    int i = arr[0] - 1;
+
+    while (i != '\0')
+    {
+        if (arr[i] > max)
+        { // dealing with max
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        { // dealing with min
+            min = arr[i];
+        }
+        i++;
+
+    }
+
+    printf("The min is %i", min);
+    printf("The max is %i", max);
+}
+
+int main()
+{
+
+    int array[] = {1, 2, 4, 5, 6};
+    findV(array);
+    return 0;
+}
